ac238e594e7a2f6e63e6688c388848b6c8bba49e
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-20 14:00:13 -0500

    First working route for rails!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  # get "welcome" => "home#index" as => "home"
+  get "welcome" => "home#primero" # as => "home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
