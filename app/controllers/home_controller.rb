ed0d78007d295b1d3a334601b1c6a9d426c0eb83
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-22 15:35:17 -0500

    Added database configuration.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def primero
     #this is basically your route. Put your ruby here as if it were a sinatra route. 
+    @slides = Slide.all
+      @total_slides = @slides.length
+      json_slides = @slides
   end
 end
